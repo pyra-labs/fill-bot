12d9c8da1a2ba8768ddf90887d5d94882a1af12e
Iarla Crewe <iarla@quartzpay.io>
2025-09-13 15:46:05 +0100

    Skip suspect vaults

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,10 @@
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 export const MIN_LAMPORTS_BALANCE = 0.5 * LAMPORTS_PER_SOL;
+
+export const SUSPECT_VAULTS = [
+	"Ggyof64bAF7bvSxuWua4KVY9a8styJVBsykRFtMcNguK",
+	"CHfbPN3TdRGqhnzSRrHsugxS3ZFstffdXbBtCXvrqc8w",
+	"6n9gA6TEVpbh9X1n8owi23bVS8BReZAW7dq5ks5onaY4",
+	"HNECnV7RxZHWsPuHiTTQkCPELVvNmrfodFXpvBv9uMWz",
+];
