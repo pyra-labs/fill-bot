7318ec6be8832efd5f1e6792d3a740185bb38d6c
Iarla Crewe <iarla@quartzpay.io>
2025-09-17 22:18:20 +0100

    Remove ignored user

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,10 +1,3 @@
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 export const MIN_LAMPORTS_BALANCE = 0.5 * LAMPORTS_PER_SOL;
-
-export const IGNORED_USERS = [
-	"J4Cb6HRhUCLzXMPcDJA9LCgYgba9nzn4LeusKsjNUqEE",
-	"2NuCJXEJzohxnSPSc1ieWTnyTSx1VSEzF8PGnoshPqYg",
-	"JAG2tMjLG2HHBP5aRY59yC1BaWHh3ZCQLFqGhmNxuVJW",
-	"6irK1ird7brAhYHziw5Xx48HrYUYxofQQA7JJvrYjyrs",
-];
